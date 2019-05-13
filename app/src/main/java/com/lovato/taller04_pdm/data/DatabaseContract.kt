0e7c78506102a78f2bdb2fcb47700de04cefcb22
RFuentes22 <00062217@uca.edu.sv>
2019-05-13 13:06:41 -0600

    Fxed object

--- app/src/main/java/com/lovato/taller04_pdm/data/DatabaseContract.kt
+++ app/src/main/java/com/lovato/taller04_pdm/data/DatabaseContract.kt
@@ -8,7 +8,7 @@ object DatabaseContract {
     // TODO (4) Para cada tabla en la base de datos se define una entrada (Entry)
     // TODO (4.1) Cada entrada debe definir el nombre de la tabla y el de cada columna
     // TODO (4.2) Debe de heredar de BaseColumns. Para heredar el nombre de las columnas tradicionales como _ID, _COUNT y otros comportamientos básicos.
-    object PersonaEntry : BaseColumns { // Se guardan los datos relevantes de la tabla, como su nombre y sus campos.
+    object CoinEntry : BaseColumns { // Se guardan los datos relevantes de la tabla, como su nombre y sus campos.
 
         const val TABLE_NAME = "Coin"
 
